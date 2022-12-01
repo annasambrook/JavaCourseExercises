681894b5f1f7088eb76c0f83ab5a4fe3585325a5
Anna Sambrook <83281726+annasambrook@users.noreply.github.com>
2022-12-01 15:41:17 +0000

    Add files via upload

--- BankingApp/src/main/java/com/anna/entity/User.java
+++ BankingApp/src/main/java/com/anna/entity/User.java
@@ -1,5 +1,6 @@
 package com.anna.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -16,6 +17,21 @@ import lombok.NoArgsConstructor;
 public class User {
 
 	@Id
+	@Column(name = "accountId")
 	private String userName;
+	
+	@Column(name = "customerPassword")
 	private String password;
+	
+	@Column(name = "customerName")
+	private String customerName;
+	
+	@Column(name = "address")
+	private String address;
+	
+	@Column(name = "phoneNumber")
+	private int phoneNumber;
+	
+	@Column(name = "bankBalance")
+	private double bankBalance;
 }
